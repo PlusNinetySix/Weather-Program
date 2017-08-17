99fdc1a17b45f0c50333e43923ada35edcd386b8
JGPNS <PlusNinetySix@users.noreply.github.com>
2017-08-17 14:00:13 -0500

    Updated grammar

--- WeatherReport.java
+++ WeatherReport.java
@@ -8,7 +8,7 @@ public class WeatherReport
 		int temp=cin.nextInt();
 		if (temp>=90)
 		{
-			System.out.println("That's pretty hot!");
+			System.out.println("That's really hot!");
 		}
 		else if (temp<=32)
 		{
